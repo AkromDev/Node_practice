05070c27ed3b99d3c55c9844e684ec454b946ecd
Akrom <akrom@sovereignwallet.network>
2019-11-16 23:55:39 +0900

    Express basic middleware

--- app.js
+++ app.js
@@ -1,52 +1,15 @@
-const http = require('http')
+const http = require("http")
+const express = require("express")
 
-const server = http.createServer((req, res) => {
-  const { url, method } = req;
-  res.setHeader('Content-Type', 'text/html')
-  if (url === '/form') {
-    res.write(
-      `<html>
-              <head><title>Yo My Website</title></head>
-              <body>
-              <form action="/message" method="POST">
-                <input type="text" name="message">
-                <button type="submit">Submit Dude</button>
-               </form>
-              </body>
-            </html>
-            `
-    )
-    return res.end()
-  }
-  if (url === '/message' && method === 'POST') {
-    res.write(
-      `<html>
-              <head><title>Yo My Website</title></head>
-              <body>
-              <div>
-                 <h2>Submitted !!!</h2>
-                 <a href="/">Go Back Dude</a>
-              </div>
-              </body>
-            </html> 
-            `
-    )
-    return res.end()
-  }
-  res.write(
-    `<html>
-          <head><title>Yo My Website</title></head>
-          <body>
-            <h1>Sup</h1>
-            <h2>DAmn Bro</h2>
-            <p>Look great</p>
-            <a href="/form">Submit ur message</a>
-          </body>
-        </html>
-        `
-  )
-  res.end()
+const app = express()
+app.use('/about', (req, res, next) => {
+  console.log('You will be directed to home page')
+  next()
+})
+app.use('/', (req, res) => {
+  res.send('<h2>Sup my dude ? <h/2>')
 })
 
+const server = http.createServer(app)
 const PORT = 4000;
 server.listen(PORT, () => console.log(`Started server at port ${PORT}`))
