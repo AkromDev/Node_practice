6e1289c3a388dc4dd2b48935dd4bcb65c5c06cd6
Akrom <akrom@sovereignwallet.network>
2019-11-24 22:44:32 +0900

    Converted to html pages

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,11 @@
+const path = require('path')
+
 const express = require('express')
 
 const router = express.Router()
 
 router.get('/add-product',(req, res) => {
-    res.send(
-      `<form action="/admin/add-product" method="POST">
-        <label for="product">Add Product</label>
-        <input name="product"/>
-        <button type="submit">Add</button>
-      </form>
-    `)
+    res.sendFile(path.join(__dirname,'../','views','add-product.html'))
   })
   router.post('/add-product', (req,res) => {
       console.log('req ', req.body.product)
--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,10 @@
+const path = require('path')
+
 const express = require('express')
 
 const router = express.Router()
 
 router.get(('/'),(req, res) => {
-    res.send(
-      `<h2>Sup my dude ? <h/2>
-      <br/>
-      <a href="/admin/add-product">Add Product</a>
-    `)
+    res.sendFile(path.join(__dirname,'../','views','shop.html'))
   })
 module.exports = router;
