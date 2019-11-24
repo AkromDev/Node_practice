99ab93d49e8a83a8db75005eed7c4d79a11d5ad5
Akrom <akrom@sovereignwallet.network>
2019-11-25 00:24:56 +0900

    Pug added

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-const http = require("http")
-const path = require("path")
-const express = require("express")
-const bodyParser = require('body-parser')
-const shopRoute = require('./routes/shop')
-const adminRoute = require('./routes/admin')
+const path = require('path');
 
-const app = express()
-app.use(bodyParser.urlencoded({ extended: false }))
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoute)
-app.use(shopRoute)
-app.use((req,res) => {
-  res.send('<h2>Page not found, dude</h2>')
-})
-const server = http.createServer(app)
-const PORT = 3000;
-server.listen(PORT, () => console.log(`Started server at port ${PORT}`))
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+});
+
+app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,23 @@
-const path = require('path')
+const path = require('path');
 
-const express = require('express')
+const express = require('express');
 
-const router = express.Router()
-const rootDir = require('../utils/path');
+const rootDir = require('../util/path');
 
-router.get('/add-product',(req, res) => {
-    res.sendFile(path.join(rootDir,'views','add-product.html'))
-  })
-  router.post('/add-product', (req,res) => {
-      console.log('req ', req.body.product)
-      res.redirect('/')
-  })
-module.exports = router;
+const router = express.Router();
+
+const products = [];
+
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
+});
+
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect('/');
+});
+
+exports.routes = router;
+exports.products = products;
