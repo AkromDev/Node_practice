0871c8396ff9b26bf32bb385143547a182eefc07
Akrom <akromsprinter@gmail.com>
2020-07-18 22:29:52 +0900

    changes

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-const http = require("http")
-const express = require("express")
-const bodyParser = require('body-parser')
+const http = require("http");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const app = express()
-app.use(bodyParser.urlencoded({ extended: false }))
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/about', (req, res, next) => {
+app.use("/about", (req, res, next) => {
   res.send(
-    `<h2>Sup homie in about ? </h2>
+    `<h2>About</h2>
     <a href="/">Go home</a>
-    `)
-})
-app.get('/form', (req, res, next) => {
+    `
+  );
+});
+app.get("/form", (req, res, next) => {
   res.send(
     `<form action="/message" method="POST">
         <label for="message">Message</label>
@@ -20,23 +21,24 @@ app.get('/form', (req, res, next) => {
     </form>
     <a href="/">Back Home</a>    
     `
-  )
-})
-app.post('/message', (req, res, next) => {
-  console.log('request body ', req.body)
+  );
+});
+app.post("/message", (req, res, next) => {
+  console.log("request body ", req.body);
   res.send(
-    `<h1>You have submitted ur trash successfully, thanks</h1>
+    `<h1>Your message is <span style="color:aqua">${req.body.message}</span></h1>
     <a href="/">Go Home Now</a>    
     `
-  )
-})
-app.use('/', (req, res) => {
+  );
+});
+app.use("/", (req, res) => {
   res.send(
-    `<h2>Sup my dude ? <h/2>
-    <a href="/form">Submit ur trash</a>
-  `)
-})
+    `<h2>Home page <h/2>
+    <a href="/form" style="display: block;">Submit your message</a>
+  `
+  );
+});
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 const PORT = 4000;
-server.listen(PORT, () => console.log(`Started server at port ${PORT}`))
+server.listen(PORT, () => console.log(`Started server at port ${PORT}`));
