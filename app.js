01eaf7a0645f1c44c1c36a5c1cf0de2f98dcbdd9
Akrom <akromsprinter@gmail.com>
2020-07-19 11:47:25 +0900

    .

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const db = require('./util/database')
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -12,12 +11,6 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-
-db.execute('SELECT * from products').then(res => {
-    console.log('res is ',res[0])
-}).catch(err => {
-    console.log('err is ',err)
-})
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
