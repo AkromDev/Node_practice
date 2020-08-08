1d9df2e951a991add4657887c15d561c787d65dc
Akrom <akromsprinter@gmail.com>
2020-08-08 20:34:35 +0900

    mongoose fully working

--- app.js
+++ app.js
@@ -1,37 +1,61 @@
-const path = require('path')
+const path = require('path');
 
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-const errorController = require('./controllers/error')
-const mongoConnect = require('./util/database').mongoConnect
-const User = require('./models/user')
+const errorController = require('./controllers/error');
+const User = require('./models/user');
 
-const app = express()
+const app = express();
 
-app.set('view engine', 'ejs')
-app.set('views', 'views')
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById('5f2e78534dbd5a480ed09eb5')
+  User.findById('5f2e8bddb8bff94df6fab0d4')
     .then(user => {
-      req.user = new User(user.name, user.email, user.cart || {items:[]}, user._id)
-      next()
+      req.user = user;
+      next();
     })
-    .catch(err => console.log(err))
-})
+    .catch(err => console.log(err));
+});
 
-app.use('/admin', adminRoutes)
-app.use(shopRoutes)
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.use(errorController.get404)
+app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(4000)
-})
+mongoose
+  .connect(
+    'mongodb+srv://akrom:akrom@cluster0.3sj7k.mongodb.net/shop?retryWrites=true&w=majority',
+    { 
+      useNewUrlParser: true,
+      useUnifiedTopology: true 
+    }
+  )
+  .then(result => {
+    User.findOne().then(user => {
+      console.log('user',user)
+      if (!user) {
+        const user = new User({
+          name: 'Akrom',
+          email: 'akrom@test.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
+    app.listen(4000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
