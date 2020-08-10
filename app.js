586d0c12ede712acc37d2263e94f80c30a67e627
Akrom <akromsprinter@gmail.com>
2020-08-11 06:56:10 +0900

    reset password working fully

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
-const path = require('path');
+const path = require('path')
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const MongoDBStore = require('connect-mongodb-session')(session);
-const csrf = require('csurf');
-const flash = require('connect-flash');
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const session = require('express-session')
+const MongoDBStore = require('connect-mongodb-session')(session)
+const csrf = require('csurf')
+const flash = require('connect-flash')
 
-const errorController = require('./controllers/error');
-const User = require('./models/user');
+const errorController = require('./controllers/error')
+const User = require('./models/user')
 
 const MONGODB_URI =
-  'mongodb+srv://akrom:akrom@cluster0.3sj7k.mongodb.net/shop?retryWrites=true&w=majority';
+  'mongodb+srv://akrom:akrom@cluster0.3sj7k.mongodb.net/shop?retryWrites=true&w=majority'
 
-const app = express();
+const app = express()
 const store = new MongoDBStore({
   uri: MONGODB_URI,
   collection: 'sessions'
-});
-const csrfProtection = csrf();
+})
+const csrfProtection = csrf()
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set('view engine', 'ejs')
+app.set('views', 'views')
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const authRoutes = require('./routes/auth');
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+const authRoutes = require('./routes/auth')
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(
   session({
     secret: 'my secret',
@@ -37,45 +37,45 @@ app.use(
     saveUninitialized: false,
     store: store
   })
-);
-app.use(csrfProtection);
-app.use(flash());
+)
+app.use(csrfProtection)
+app.use(flash())
 
 app.use((req, res, next) => {
   if (!req.session.user) {
-    return next();
+    return next()
   }
   User.findById(req.session.user._id)
     .then(user => {
-      req.user = user;
-      next();
+      req.user = user
+      next()
     })
-    .catch(err => console.log(err));
-});
+    .catch(err => console.log(err))
+})
 
 app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.session.isLoggedIn;
-  res.locals.csrfToken = req.csrfToken();
-  next();
-});
+  res.locals.isAuthenticated = req.session.isLoggedIn
+  res.locals.csrfToken = req.csrfToken()
+  next()
+})
 
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
-app.use(authRoutes);
+app.use('/admin', adminRoutes)
+app.use(shopRoutes)
+app.use(authRoutes)
 
-app.use(errorController.get404);
+app.use(errorController.get404)
 
 mongoose
   .connect(
     MONGODB_URI,
-    { 
+    {
       useNewUrlParser: true,
-      useUnifiedTopology: true 
+      useUnifiedTopology: true
     }
-    )
-    .then(result => {
-    app.listen(4000);
+  )
+  .then(result => {
+    app.listen(4000)
   })
   .catch(err => {
-    console.log(err);
-  });
+    console.log(err)
+  })
